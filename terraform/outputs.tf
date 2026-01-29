cdac96e4eab01e80b35520f7d749e218870d3dca
Brian Boudrioux <bboudrioux@gmail.com>
2026-01-29 06:49:45 +0100

    chore(infra): disable opensearch during aws account activation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,13 @@ output "instance_public_ip" {
 output "ssh_connection_command" {
   value = "ssh -i ${local_sensitive_file.pem_file.filename} ubuntu@${aws_instance.app_server.public_ip}"
 }
+
+# output "elasticsearch" {
+#   description = "URL de la base de données ElasticSearch"
+#   value       = aws_opensearch_domain.iac-aws-elasticsearch.endpoint
+# }
+
+# output "kibana" {
+#   description = "URL de connexion à l'instance Kibana"
+#   value       = aws_opensearch_domain.iac-aws-elasticsearch.dashboard_endpoint
+# }
